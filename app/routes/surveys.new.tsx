e31dd09bf7b46f7cf188a388d8ddd6deac2f40b7
Brian Smiley <briantsmiley42@gmail.com>
2024-06-08 01:25:37 -0400

    maybe got posting surveys working?

--- app/routes/surveys.new.tsx
+++ app/routes/surveys.new.tsx
@@ -1,20 +1,17 @@
-import { PrismaClient, Question, Survey } from "@prisma/client";
-import { Form, Link } from "@remix-run/react";
-const prisma = new PrismaClient();
-
-import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node"; // or cloudflare/deno
-import { json } from "@remix-run/node"; // or cloudflare/deno
-import { useLoaderData } from "@remix-run/react";
+import { Question, Survey } from "@prisma/client";
 import { useState } from "react";
 
-type WithoutId<T> = Omit<T, "id">;
-type NewSurveyParams = WithoutId<Survey>;
-type NewQuestionParams = WithoutId<Question>;
+export type NewSurvey = Omit<Survey, "id">;
+export type NewQuestion = Omit<Question, "id" | "surveyId">;
+export type NewSurveyPostParams = {
+  newSurvey: NewSurvey;
+  newSurveyQuestions: NewQuestion[];
+};
 
-const addNewSurveyToDatabase = async (survey: NewSurveyParams) => {
+const addNewSurveyToDatabase = async (newSurvey: NewSurveyPostParams) => {
   const response = await fetch("http://localhost:4000/create", {
     method: "POST",
-    body: JSON.stringify(survey),
+    body: JSON.stringify(newSurvey),
     headers: new Headers({ "content-type": "application/json" })
   });
 
@@ -25,17 +22,37 @@ const addNewSurveyToDatabase = async (survey: NewSurveyParams) => {
 export default function NewSurveyForm() {
   const [surveyName, setSurveyName] = useState("");
   const [questions, setQuestions] = useState<string[]>([""]);
-  const clickSubmitFunction = (surveyName: string) => () => {
-    addNewSurveyToDatabase({ name: surveyName });
+
+  //on-click to send the state variables off to db
+  const clickSubmitFunction = () => () => {
+    //turn questions array into array of Question-table-ready-objects and the survey name into a Survey-Table-Ready object
+    const preppedSurvey = { name: surveyName };
+    const preppedQuestions: NewQuestion[] = questions.map((question, idx) => ({
+      questionNumber: idx,
+      content: question
+    }));
+
+    addNewSurveyToDatabase({
+      newSurvey: preppedSurvey,
+      newSurveyQuestions: preppedQuestions
+    });
+
+    //Reset state
     setSurveyName("");
+    setQuestions([""]);
   };
+
+  //controlled-input function that correlates a quesiton's input field with its element in the questions state array
   const updateQuestionByIndexFunction = (idx: number, newQuestion: string) =>
     setQuestions(
       questions.map((question, questionIndex) =>
         questionIndex === idx ? newQuestion : question
       )
     );
+
+  //Add a new, empty, question to state
   const addNewQuestion = () => setQuestions(questions.concat([""]));
+
   return (
     <div className="flex flex-col gap-2">
       <div className="m-2 flex gap-2">
@@ -64,16 +81,16 @@ export default function NewSurveyForm() {
                 onChange={e =>
                   updateQuestionByIndexFunction(idx, e.target.value)
                 }
-              ></input>
+              />
             </div>
           );
         })}
       </div>
-      <button className="btn w-20" onClick={addNewQuestion}>
+      <button className="btn w-fit" onClick={addNewQuestion}>
         Add Question
       </button>
-      <button className="btn w-20" onClick={clickSubmitFunction(surveyName)}>
-        Create
+      <button className="btn w-20" onClick={clickSubmitFunction()}>
+        Submit
       </button>
     </div>
   );
--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import { Question, Survey, SurveyResponse } from "@prisma/client";
+import { NewSurveyPostParams } from "~/routes/surveys.new";
 import prisma from "~/client";
 import cors from "cors";
 const app = express();
@@ -10,13 +11,35 @@ app.use(express.json());
 
 app.post(
   "/create",
-  async (req: Request<{}, {}, Omit<Survey, "id">>, res: Response) => {
-    if (!req.body.name) res.status(400).send();
-    const newSurvey = await prisma.survey.create({
-      data: req.body
-    });
+  async (req: Request<{}, {}, NewSurveyPostParams>, res: Response) => {
+    //get and log reques body
+    const body = req.body;
+    console.log(body);
 
-    res.json({ newSurvey });
+    //extract the to-be-added survey and questions array
+    const { newSurvey, newSurveyQuestions } = body;
+
+    //If there isnt a new survey name on the body we gots a problem
+    if (!newSurvey) res.status(400).send();
+
+    //Create the new survey
+    const newlyCreatedSurvey = await prisma.survey.create({
+      data: newSurvey
+    });
+    //Get the survey's id in database to assign new questions to it
+    const newSurveyId = newlyCreatedSurvey.id;
+
+    //Iterate through questions array and assign each one to
+    const newlyCreatedQuestions = [];
+    for (const newQuestion of newSurveyQuestions) {
+      const createdQuestion = await prisma.question.create({
+        data: Object.assign(newQuestion, { surveyId: newSurveyId })
+      });
+      //build the newly created question array for response
+      newlyCreatedQuestions.push(createdQuestion);
+    }
+
+    res.json({ newlyCreatedSurvey, newlyCreatedQuestions });
   }
 );
 app.get("/surveys", async (req: Request, res: Response) => {
