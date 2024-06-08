3656a83cedb5dcc817437112fdd26c77586a6775
Brian Smiley <briantsmiley42@gmail.com>
2024-06-08 19:48:50 -0400

    adds a success page

---
+++ app/routes/surveys.submit.success.tsx
@@ -0,0 +1,17 @@
+import { Link } from "@remix-run/react";
+
+export default function SuccessSplash() {
+  return (
+    <div className="flex justify-center items-center w-full">
+      <div className="bg-green-700 text-white rounded-3xl w-[60%] h-[30%] flex flex-col items-center justify-center gap-2">
+        <h1 className="flex text-5xl text-center">Success</h1>
+        <div className="flex text-xl text-center">
+          Survey answers successfully submitted!
+        </div>
+        <div className=" flex justify-center text-center">
+          <Link to="/surveys">Take another survey?</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
