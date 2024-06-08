00c5be16dd64fd54cabd1ae39ab49e89290f01eb
Brian Smiley <briantsmiley42@gmail.com>
2024-06-07 20:44:39 -0400

    makes sidebar highlight current page

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,14 +1,49 @@
 import { Link } from "@remix-run/react";
+import { useState } from "react";
+type PageLinkProps = {
+  setCurrent: () => void;
+  url: string;
+  label: string;
+  isCurrent: boolean;
+};
 
-export default function Sidebar() {
+const pages: Omit<PageLinkProps, "setCurrent" | "isCurrent">[] = [
+  {
+    url: "/surveys",
+    label: "Surveys"
+  },
+  {
+    url: "surveys/new",
+    label: "New Survey"
+  }
+];
+const PageLink = ({ setCurrent, url, label, isCurrent }: PageLinkProps) => {
+  const selectionDependentClass = isCurrent
+    ? "bg-teal-500 hover:bg-teal-600"
+    : "hover:bg-teal-800";
   return (
-    <div className=" text-xl text-white flex flex-col bg-teal-700 h-full w-fit px-4 gap-1 py-6">
-      <Link className="hover:bg-teal-800 p-2 rounded-lg" to="/surveys">
-        Surveys
-      </Link>
-      <Link className="hover:bg-teal-800 p-2 rounded-lg" to="/surveys/new">
-        New Survey
-      </Link>
+    <Link
+      className={`${selectionDependentClass} p-4 `}
+      to={url}
+      onClick={setCurrent}
+    >
+      {label}
+    </Link>
+  );
+};
+export default function Sidebar() {
+  const [currentPage, setCurrentPage] = useState("/");
+
+  return (
+    <div className=" text-xl text-white flex flex-col bg-teal-700 h-full w-fit  gap-1s">
+      {pages.map(pageInfo => (
+        <PageLink
+          {...pageInfo}
+          setCurrent={() => setCurrentPage(pageInfo.url)}
+          isCurrent={currentPage === pageInfo.url}
+          key={pageInfo.url}
+        />
+      ))}
     </div>
   );
 }
